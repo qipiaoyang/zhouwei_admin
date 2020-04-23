fde141eda1bc76de049dd2f2bdb7d7ac15da9d94
齐飘洋 <qipiaoyang@outlook.com>
2020-04-24 00:49:59 +0800

    123

--- src/utils/service_config.js
+++ src/utils/service_config.js
@@ -3,10 +3,10 @@ var VUE_APP_BASE_API = "";
 
 switch (process.env.VUE_APP_ENV) {
     case "dev":
-        VUE_APP_BASE_API = 'http://47.75.78.93/';
+        VUE_APP_BASE_API = 'http://121.36.47.82:81/';
         break;
     case "test":
-        VUE_APP_BASE_API = "http://47.75.78.93/";
+        VUE_APP_BASE_API = "http://121.36.47.82:81/";
         break;
     case "prod":
         VUE_APP_BASE_API = "https://yx-test.lemonbus.cn";
