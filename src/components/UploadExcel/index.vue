4b3837b68dc45a61cc1c30937d86944c206be93c
qipiaoyang <956634645@qq.com>
2020-05-08 01:24:24 +0800

    更新excel导入

--- src/components/UploadExcel/index.vue
+++ src/components/UploadExcel/index.vue
@@ -2,9 +2,9 @@
   <div>
     <input ref="excel-upload-input" class="excel-upload-input" type="file" accept=".xlsx, .xls" @change="handleClick">
     <div class="drop" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">
-      Drop excel file here or
+      拖拽excel至
       <el-button :loading="loading" style="margin-left:16px;" size="mini" type="primary" @click="handleUpload">
-        Browse
+        浏览...
       </el-button>
     </div>
   </div>
